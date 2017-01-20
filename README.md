3f4f50a5842a73e68fa22ea96d995f5346224b01
besseddrest <hcabalic@gmail.com>
2017-01-20 00:10:01 -0800

    init app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Harold's Online portfolio
+The portfolio that every prospective job asks for.
+
+Build with ReactJS.
---
+++ client/data/projects.js
@@ -0,0 +1,76 @@
+const projects = [
+  {
+    name: 'LOW// Bicycles',
+    shortDesc: 'Handmade aluminum bicycle frames in San Francisco',
+    longDesc: "I wanted to be part of something homegrown in San Francisco. When I moved here in 2009, the fixed gear bicycle scene was huge. I didn't think I could get any exposure as a rider, so I thought I could contribute to the cycling scene by helping Andrew Low with his website.",
+    url: 'http://lowbicycles.com/',
+    github: 'https://github.com/besseddrest/lowbicycles.com',
+    tags: ['drupal', 'cms', 'php', 'bootstrap', 'css'],
+    thumb: ''
+  },
+  {
+    name: "Danny Coyle's Super Chargers",
+    shortDesc: 'The Greatest Chargers Bar Ever',
+    longDesc: "San Francisco is home to The Greatest Chargers Bar Ever. Being a San Diego transplant, I spend many a weekend here celebrating with fellow Chargers fans. I felt it was important to capture this and show off to the rest of the world by building out their website, which include a shop that gives me the opportunity to create and sell custom goods.",
+    url: 'http://dcsuperchargers.com/',
+    github: '',
+    tags: ['drupal', 'cms', 'php', 'bootstrap', 'css'],
+    thumb: ''
+  },
+  {
+    name: 'SquareTrade',
+    shortDesc: 'Protection Plans for Electronic Devices and Appliances',
+    longDesc: 'SquareTrade was my employer from 2011-2017. Early on, I helped migrate their website from Drupal 5 to Drupal 7, and was the primary front-end developer for 2-3 website redesigns. In the later years of my career there I was involved in creating and maintaining their several Javascript applications',
+    url: 'http://www.squaretrade.com/',
+    github: '',
+    tags: ['drupal', 'cms', 'php', 'javascript', 'mvc', 'angular'],
+    thumb: ''
+  },
+  {
+    name: 'Hoodline',
+    shortDesc: 'Re-creating the Hoodline Blog with React & Redux',
+    longDesc: "I re-created the main part of Hoodline's blog in React & Redux as a small presentation during the interview process.",
+    url: '',
+    github: 'https://github.com/besseddrest/hoodline-blog',
+    tags: ['cms', 'blog', 'javascript', 'react', 'redux'],
+    thumb: ''
+  },
+  {
+    name: 'ToDo',
+    shortDesc: 'A simple ToDo app built with native JS and Sass',
+    longDesc: "",
+    url: 'http://besseddrest.com/todo',
+    github: 'https://github.com/besseddrest/todo-app',
+    tags: ['todo', 'javascript', 'es6', 'sass', 'flexbox'],
+    thumb: ''
+  },
+  {
+    name: 'Lightbox',
+    shortDesc: 'A simple Lightbox example built with native JS and Sass',
+    longDesc: "",
+    url: 'http://besseddrest.com/lightbox',
+    github: 'https://github.com/besseddrest/lightbox-app',
+    tags: ['lightbox', 'javascript', 'es6', 'sass', 'flexbox'],
+    thumb: ''
+  },
+  {
+    name: 'Tic Tac Toe',
+    shortDesc: 'My first app in React',
+    longDesc: "",
+    url: '',
+    github: 'https://github.com/besseddrest/tic-tac-toe',
+    tags: ['tictactoe', 'javascript', 'react'],
+    thumb: ''
+  },
+  {
+    name: '',
+    shortDesc: '',
+    longDesc: "",
+    url: '',
+    github: '',
+    tags: [],
+    thumb: ''
+  }
+];
+
+export default projects;
